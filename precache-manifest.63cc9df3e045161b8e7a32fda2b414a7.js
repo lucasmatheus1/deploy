72b04af78601e400a099f10c751055ccf2f709ef
lucasmatheus1 <lucassteus@gmail.com>
2020-05-26 21:36:10 -0300

    Updates

---
+++ precache-manifest.63cc9df3e045161b8e7a32fda2b414a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c3d50ae51cbd443fcabc0b6ca61f3f",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "a0beb2311517072fedb4",
+    "url": "/deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "68dda8b9de13e6891a87",
+    "url": "/deploy/static/css/main.2901cd8b.chunk.css"
+  },
+  {
+    "revision": "a0beb2311517072fedb4",
+    "url": "/deploy/static/js/2.ffbbc148.chunk.js"
+  },
+  {
+    "revision": "9c04abda40226cf1e9546f90f209d3e0",
+    "url": "/deploy/static/js/2.ffbbc148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68dda8b9de13e6891a87",
+    "url": "/deploy/static/js/main.1dbbf517.chunk.js"
+  },
+  {
+    "revision": "228f19b3acfcfcd2c582",
+    "url": "/deploy/static/js/runtime-main.f072c425.js"
+  }
+]);
